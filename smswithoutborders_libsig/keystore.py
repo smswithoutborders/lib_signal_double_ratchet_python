d1ea270bcd0d3b1241b99025e02ee2e2100ba107
Promise Fru <info@promisefru.com>
2024-12-03 10:42:05 +0100

    chore: updates license to MIT

--- smswithoutborders_libsig/keystore.py
+++ smswithoutborders_libsig/keystore.py
@@ -1,9 +1,5 @@
 """
 A module for managing a secure keystore.
- 
-This program is free software: you can redistribute it under the terms
-of the GNU General Public License, v. 3.0. If a copy of the GNU General
-Public License was not distributed with this file, see <https://www.gnu.org/licenses/>.
 """
 
 
