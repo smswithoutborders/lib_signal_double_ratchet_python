e4fc38219f546fabe3ca591c7b8dd64a4d73e85e
sherlockwisdom <wisdomnji@gmail.com>
2026-01-09 21:06:46 +0100

    update: added new method for agreeing with nonce

--- src/main/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/CryptoHelpers.java
+++ src/main/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/CryptoHelpers.java
@@ -85,9 +85,7 @@ public class CryptoHelpers {
 
     public static byte[] generateRandomBytes(int length) {
         SecureRandom random = new SecureRandom();
-        byte[] bytes = new
-
-                byte[length];
+        byte[] bytes = new byte[length];
         random.nextBytes(bytes);
         return bytes;
     }
--- src/main/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/SecurityCurve25519.kt
+++ src/main/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/SecurityCurve25519.kt
@@ -7,6 +7,43 @@ class SecurityCurve25519(val privateKey: ByteArray = Curve25519.generateRandomKe
         return Curve25519.publicKey(this.privateKey)
     }
 
+    fun agreeWithAuthAndNonce(
+        authenticationPublicKey: ByteArray,
+        publicKey: ByteArray,
+        salt: ByteArray,
+        nonce1: ByteArray,
+        nonce2: ByteArray,
+        info: ByteArray,
+    ): ByteArray {
+        val handshakeSalt = nonce1 + nonce2
+        val dh1 = Curve25519.sharedSecret(this.privateKey, authenticationPublicKey)
+        val dh2 = Curve25519.sharedSecret(this.privateKey, publicKey)
+        var ck = CryptoHelpers.HKDF(
+            "HMACSHA256",
+            handshakeSalt,
+            salt,
+            info,
+            32,
+            1
+        )[0]
+        ck = CryptoHelpers.HKDF(
+            "HMACSHA256",
+            dh1,
+            ck,
+            info,
+            32,
+            1
+        )[0]
+        return CryptoHelpers.HKDF(
+            "HMACSHA256",
+            dh2,
+            ck,
+            info,
+            32,
+            1
+        )[0]
+    }
+
     fun calculateSharedSecret(publicKey: ByteArray): ByteArray {
         val sharedKey = Curve25519.sharedSecret(this.privateKey, publicKey)
         return CryptoHelpers.HKDF("HMACSHA256", sharedKey, null,
