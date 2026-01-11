022ba12f56773d7893c786777aa5a53cce8fe582
sherlockwisdom <wisdomnji@gmail.com>
2026-01-11 17:43:55 +0100

    update: Ratchet's test all implemented and past

--- src/androidTest/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/libsignal/RatchetsTest.kt
+++ src/androidTest/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/libsignal/RatchetsTest.kt
@@ -5,6 +5,7 @@ import androidx.core.util.component1
 import androidx.core.util.component2
 import androidx.test.filters.SmallTest
 import androidx.test.platform.app.InstrumentationRegistry
+import com.afkanerd.smswithoutborders.libsignal_doubleratchet.CryptoHelpers
 import com.afkanerd.smswithoutborders.libsignal_doubleratchet.SecurityCurve25519
 import org.junit.Assert.assertArrayEquals
 import org.junit.Test
@@ -14,6 +15,108 @@ import java.security.SecureRandom
 class RatchetsTest {
     var context: Context =
         InstrumentationRegistry.getInstrumentation().targetContext
+    @Test
+    fun completeRatchetHETest() {
+        val aliceEphemeralKeyPair = SecurityCurve25519()
+        val aliceEphemeralHeaderKeyPair = SecurityCurve25519()
+        val aliceEphemeralNextHeaderKeyPair = SecurityCurve25519()
+
+        val bobStaticKeyPair = SecurityCurve25519()
+        val bobEphemeralKeyPair = SecurityCurve25519()
+        val bobEphemeralHeaderKeyPair = SecurityCurve25519()
+        val bobEphemeralNextHeaderKeyPair = SecurityCurve25519()
+
+        val aliceNonce = CryptoHelpers.generateRandomBytes(16)
+        val bobNonce = CryptoHelpers.generateRandomBytes(16)
+
+        val (aliceSk, aliceSkH, aliceSkNh) = SecurityCurve25519(aliceEphemeralKeyPair.privateKey)
+            .agreeWithAuthAndNonce(
+                authenticationPublicKey = bobStaticKeyPair.generateKey(),
+                authenticationPrivateKey = null,
+                headerPrivateKey = aliceEphemeralHeaderKeyPair.privateKey,
+                nextHeaderPrivateKey = aliceEphemeralNextHeaderKeyPair.privateKey,
+                publicKey = bobEphemeralKeyPair.generateKey(),
+                headerPublicKey = bobEphemeralHeaderKeyPair.generateKey(),
+                nextHeaderPublicKey = bobEphemeralNextHeaderKeyPair.generateKey(),
+                salt = "RelaySMS v1".encodeToByteArray(),
+                nonce1 = aliceNonce,
+                nonce2 = bobNonce,
+                info = "RelaySMS C2S DR v1".encodeToByteArray()
+            )
+
+        val (bobSk, bobSkH, bobSkNh) = SecurityCurve25519(bobEphemeralKeyPair.privateKey)
+            .agreeWithAuthAndNonce(
+                authenticationPublicKey = null,
+                authenticationPrivateKey = bobStaticKeyPair.privateKey,
+                headerPrivateKey = bobEphemeralHeaderKeyPair.privateKey,
+                nextHeaderPrivateKey = bobEphemeralNextHeaderKeyPair.privateKey,
+                publicKey = aliceEphemeralKeyPair.generateKey(),
+                headerPublicKey = aliceEphemeralHeaderKeyPair.generateKey(),
+                nextHeaderPublicKey = aliceEphemeralNextHeaderKeyPair.generateKey(),
+                salt = "RelaySMS v1".encodeToByteArray(),
+                nonce1 = aliceNonce,
+                nonce2 = bobNonce,
+                info = "RelaySMS C2S DR v1".encodeToByteArray()
+            )
+
+        assertArrayEquals(aliceSk, bobSk)
+        assertArrayEquals(aliceSkH, bobSkH)
+        assertArrayEquals(aliceSkNh, bobSkNh)
+
+        val aliceState = States()
+        RatchetsHE.ratchetInitAlice(
+            state = aliceState,
+            SK = aliceSk,
+            bobDhPublicKey = bobEphemeralKeyPair.generateKey(),
+            sharedHka = aliceSkH,
+            sharedNhkb = aliceSkNh
+        )
+
+        val bobState = States()
+        RatchetsHE.ratchetInitBob(
+            state = bobState,
+            SK = bobSk,
+            bobDhPublicKeypair = bobEphemeralKeyPair.getKeypair(),
+            sharedHka = bobSkH,
+            sharedNhkb = bobSkNh
+        )
+
+        val originalText = SecureRandom.getSeed(32);
+        val (encHeader, aliceCipherText) = RatchetsHE.ratchetEncrypt(
+            aliceState,
+            originalText,
+            bobStaticKeyPair.generateKey()
+        )
+
+        var encHeader1: ByteArray? = null
+        var aliceCipherText1: ByteArray? = null
+        for(i in 1..10) {
+            val (encHeader2, aliceCipherText2) = RatchetsHE.ratchetEncrypt(
+                aliceState,
+                originalText,
+                bobStaticKeyPair.generateKey()
+            )
+            encHeader1 = encHeader2
+            aliceCipherText1 = aliceCipherText2
+        }
+
+        val bobPlainText = RatchetsHE.ratchetDecrypt(
+            state = bobState,
+            encHeader = encHeader,
+            cipherText = aliceCipherText,
+            AD = bobStaticKeyPair.generateKey()
+        )
+
+        val bobPlainText1 = RatchetsHE.ratchetDecrypt(
+            state = bobState,
+            encHeader = encHeader1!!,
+            cipherText = aliceCipherText1!!,
+            AD = bobStaticKeyPair.generateKey()
+        )
+
+        assertArrayEquals(originalText, bobPlainText)
+        assertArrayEquals(originalText, bobPlainText1)
+    }
 
     @Test
     fun completeRatchetTest() {
--- src/main/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/SecurityCurve25519.kt
+++ src/main/java/com/afkanerd/smswithoutborders/libsignal_doubleratchet/SecurityCurve25519.kt
@@ -7,17 +7,21 @@ class SecurityCurve25519(val privateKey: ByteArray = Curve25519.generateRandomKe
         return Curve25519.publicKey(this.privateKey)
     }
 
-    fun agreeWithAuthAndNonce(
-        authenticationPublicKey: ByteArray,
+    private fun agreeWithAuthAndNonceImpl(
+        authenticationPublicKey: ByteArray?,
+        authenticationPrivateKey: ByteArray?,
         publicKey: ByteArray,
         salt: ByteArray,
-        nonce1: ByteArray,
-        nonce2: ByteArray,
         info: ByteArray,
+        handshakeSalt: ByteArray,
+        privateKey: ByteArray? = null,
     ): ByteArray {
-        val handshakeSalt = nonce1 + nonce2
-        val dh1 = Curve25519.sharedSecret(this.privateKey, authenticationPublicKey)
-        val dh2 = Curve25519.sharedSecret(this.privateKey, publicKey)
+        val privateKey = privateKey ?: this.privateKey
+        val dh1 = if(authenticationPrivateKey == null)
+            Curve25519.sharedSecret(privateKey, authenticationPublicKey)
+        else
+            Curve25519.sharedSecret(authenticationPrivateKey, publicKey)
+        val dh2 = Curve25519.sharedSecret(privateKey, publicKey)
         var ck = CryptoHelpers.HKDF(
             "HMACSHA256",
             handshakeSalt,
@@ -42,6 +46,54 @@ class SecurityCurve25519(val privateKey: ByteArray = Curve25519.generateRandomKe
             32,
             1
         )[0]
+    }
+
+    fun agreeWithAuthAndNonce(
+        authenticationPublicKey: ByteArray?,
+        authenticationPrivateKey: ByteArray?,
+        headerPrivateKey: ByteArray,
+        nextHeaderPrivateKey: ByteArray,
+        publicKey: ByteArray,
+        headerPublicKey: ByteArray,
+        nextHeaderPublicKey: ByteArray,
+        salt: ByteArray,
+        nonce1: ByteArray,
+        nonce2: ByteArray,
+        info: ByteArray,
+    ): Triple<ByteArray, ByteArray, ByteArray> {
+        val handshakeSalt = nonce1 + nonce2
+        val headerInfo = "RelaySMS C2S DRHE v1".encodeToByteArray()
+
+        val rootKey = agreeWithAuthAndNonceImpl(
+            authenticationPublicKey = authenticationPublicKey,
+            authenticationPrivateKey = authenticationPrivateKey,
+            publicKey = publicKey,
+            salt = salt,
+            info = info,
+            handshakeSalt = handshakeSalt,
+        )
+
+        val headerKey = agreeWithAuthAndNonceImpl(
+            authenticationPublicKey = authenticationPublicKey,
+            authenticationPrivateKey = authenticationPrivateKey,
+            publicKey = headerPublicKey,
+            salt = salt,
+            info = headerInfo,
+            handshakeSalt = handshakeSalt,
+            privateKey = headerPrivateKey
+        )
+
+        val nextHeaderKey = agreeWithAuthAndNonceImpl(
+            authenticationPublicKey = authenticationPublicKey,
+            authenticationPrivateKey = authenticationPrivateKey,
+            publicKey = nextHeaderPublicKey,
+            salt = salt,
+            info = headerInfo,
+            handshakeSalt = handshakeSalt,
+            privateKey = nextHeaderPrivateKey
+        )
+
+        return Triple(rootKey, headerKey, nextHeaderKey)
     }
 
     fun calculateSharedSecret(
